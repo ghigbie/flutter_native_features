d286fec266ce718ae722ad44fb076f21b44f50a1
ghigbie <gehigbie@gmail.com>
2019-06-08 18:41:02 -0600

    Add more android imports

--- android/app/src/main/java/com/example/native_features/MainActivity.java
+++ android/app/src/main/java/com/example/native_features/MainActivity.java
@@ -9,10 +9,29 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+
 public class MainActivity extends FlutterActivity {
+
+  private static final String CHANNEL = "devslopes.native_features/battery";
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+            new MethodCallHandler() {
+              @Override
+              public void onMethodCall(MethodCall methodCall, Result result) {
+                //Handel
+              }
+            }
+    );
   }
 }
